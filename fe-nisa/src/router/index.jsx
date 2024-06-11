6eb565b5847ed8c27f5a294cb94d020dbd793587
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-06-11 11:23:18 +0700

    styling create almost done

--- fe-nisa/src/router/index.jsx
+++ fe-nisa/src/router/index.jsx
@@ -4,12 +4,17 @@ import Home from "../pages/Home";
 import HpmDetail from '../pages/HpmDetail';
 import CreateHomepass from '../pages/CreateHomepass';
 import EditHomepass from '../pages/EditHomepass';
+import Template from '../pages/Template';
 
 
 const router = createBrowserRouter([
     {
       element: <Parent />,
       children: [
+        {
+          path: "/tes",
+          element: <Template />,
+        },
         {
           path: "/",
           element: <Home />,
