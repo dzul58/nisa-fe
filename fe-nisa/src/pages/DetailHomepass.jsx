9e96295a696a6e3bad2344bf0edfebf321162cd4
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-06-27 13:45:14 +0700

    cleaning code

--- fe-nisa/src/pages/DetailHomepass.jsx
+++ fe-nisa/src/pages/DetailHomepass.jsx
@@ -173,8 +173,7 @@ const DetailHomepass = () => {
               <div className="mt-2">
                 <input type="text" id="hpm_check_result" name="hpm_check_result" value={data.hpm_check_result} readOnly className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
-            </div>
-
+          </div>
 
 
           <div className="sm:col-span-3">
