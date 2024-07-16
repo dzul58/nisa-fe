8888486282ac96443e78fcc6c5975974bb5194c0
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-07-16 09:37:38 +0700

    change port EditHomepass.jsx  done

--- fe-nisa/src/pages/EditHomepass.jsx
+++ fe-nisa/src/pages/EditHomepass.jsx
@@ -27,7 +27,7 @@ const EditHomepass = () => {
   useEffect(() => {
     const fetchHomepassData = async () => {
       try {
-        const response = await axios.get(`http://localhost:8000/api/homepass/${id}`, {
+        const response = await axios.get(`https://moving-address-be.oss.myrepublic.co.id/api/homepass/${id}`, {
           headers: {
             Authorization: `Bearer ${localStorage.access_token}`,
           },
@@ -61,7 +61,7 @@ const EditHomepass = () => {
         ...photoUploadResults
       };
 
-      const response = await axios.put(`http://localhost:8000/api/edit-homepass/${id}`, dataToSend, {
+      const response = await axios.put(`https://moving-address-be.oss.myrepublic.co.id/api/edit-homepass/${id}`, dataToSend, {
         headers: {
           Authorization: `Bearer ${localStorage.access_token}`,
         },
@@ -100,7 +100,7 @@ const EditHomepass = () => {
         const photoFormData = new FormData();
         photoFormData.append(photo.key, formData[photo.key]);
         try {
-          const uploadResponse = await axios.post(`http://localhost:8000/api/${photo.endpoint}`, photoFormData, {
+          const uploadResponse = await axios.post(`https://moving-address-be.oss.myrepublic.co.id/api/${photo.endpoint}`, photoFormData, {
             headers: {
               "Content-Type": "multipart/form-data",
               Authorization: `Bearer ${localStorage.access_token}`,
