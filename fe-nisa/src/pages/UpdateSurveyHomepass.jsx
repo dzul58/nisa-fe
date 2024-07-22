be5173afc0b2a3e71f90661f4967f569c676baa4
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-07-22 13:43:28 +0700

    added pages survey ops

---
+++ fe-nisa/src/pages/UpdateSurveyHomepass.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const UpdateSurveyHomepass = () => {
+
+  return (
+    <></>
+);
+}
+
+export default UpdateSurveyHomepass;
