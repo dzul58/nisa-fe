2c2187096bf11dda0aeba4bf92f2397e88469e53
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-07-26 11:03:59 +0700

    details video done

--- fe-nisa/src/pages/DetailHomepass.jsx
+++ fe-nisa/src/pages/DetailHomepass.jsx
@@ -643,21 +643,21 @@ const DetailHomepass = () => {
                 </div>
 
                 <div className="col-span-3">
-  <label htmlFor="photo4_survey_ops" className="block text-sm font-medium leading-6 text-gray-900">
+  <label htmlFor="video_survey_ops" className="block text-sm font-medium leading-6 text-gray-900">
     Video Survey Ops
   </label>
   <div className="relative inline-block group flex items-center space-x-2">
     <input 
       type="text" 
-      id="photo4_survey_ops" 
-      name="photo4_survey_ops" 
-      value={data.photo4_survey_ops || ''} 
+      id="video_survey_ops" 
+      name="video_survey_ops" 
+      value={data.video_survey_ops || ''} 
       readOnly 
       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
     />
     <button 
       type="button"
-      onClick={() => openVideoModal(data.photo4_survey_ops)}
+      onClick={() => openVideoModal(data.video_survey_ops)}
       className="px-4 py-1 text-lg font-medium bg-[#662b81] text-white rounded-lg cursor-pointer"
     >
       Video
