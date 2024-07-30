91f460a12aea9c9833c858d6556d7825696985a2
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-07-30 14:43:03 +0700

    new dashboard kpi

--- fe-nisa/src/pages/ErrorPage.jsx
+++ fe-nisa/src/pages/ErrorPage.jsx
@@ -2,7 +2,7 @@ import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
+  // console.error(error);
 
   return (
     <div id="error-page">
@@ -14,5 +14,3 @@ export default function ErrorPage() {
     </div>
   );
 }
-
-
