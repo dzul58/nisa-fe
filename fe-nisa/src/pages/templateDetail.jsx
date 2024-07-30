6b39f67084495c9bbaa23617a0b8c0aa5abed16d
Dzulfiqar Akmaluddin <dzulfiqar5819@gmail.com>
2024-07-30 14:42:01 +0700

    new dashboard kpi

--- fe-nisa/src/pages/templateDetail.jsx
+++ fe-nisa/src/pages/templateDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { format } from 'date-fns';
 import { id } from 'date-fns/locale';
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const DetailHomepass = () => {
   const { id: detailId } = useParams();
